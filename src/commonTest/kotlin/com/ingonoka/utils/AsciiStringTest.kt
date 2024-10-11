e545330385790357edc1932742582da23ca52c7b
ingonoka <ingonoka@icloud.com>
2024-10-11 14:06:37 +0800

    Make AsciiString serializable

--- src/commonTest/kotlin/com/ingonoka/utils/AsciiStringTest.kt
+++ src/commonTest/kotlin/com/ingonoka/utils/AsciiStringTest.kt
@@ -1,10 +1,32 @@
 package com.ingonoka.utils
 
+import com.ingonoka.hexutils.hexToBytes
+import kotlinx.serialization.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.protobuf.ProtoBuf
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 
 class AsciiStringTest {
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Test
+    fun serializeProtoBuf() {
+
+        @Serializable
+        data class Test(val s: AsciiString)
+
+        val t = Test(AsciiString("A"))
+        val ba = "0a0141".hexToBytes()
+        val json = "{\"s\":\"A\"}"
+        assertContentEquals( ba , ProtoBuf.encodeToByteArray(t))
+        assertEquals( t, ProtoBuf.decodeFromByteArray<Test>(ba))
+
+        assertEquals(json, Json.encodeToString(t))
+        assertEquals(t, Json.decodeFromString<Test>(json))
+
+    }
 
     @Test
     fun testConstructor() {
